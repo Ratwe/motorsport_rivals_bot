2660efeb3c53bbf36d580221fdf02a65a93d499a
Artem Dyachenko <artemon1324@yandex.ru>
2023-08-08 00:44:09 +0300

    ADD: handle_messages()

--- classes.py
+++ classes.py
@@ -1,6 +1,13 @@
+class UserState:
+    def __init__(self):
+        self.entering_club = False
+        self.entering_teams = False
+        self.entering_overtakes = False
+
+
 class Lap:
     def __init__(self, lap_data):
-        self.lap_number = lap_data['lap_number']
+        self.number = lap_data['lap_number']
         self.speed_team1 = lap_data['speed_team1']
         self.speed_team2 = lap_data['speed_team2']
         self.best_lap = lap_data.get('best_lap', False)
@@ -10,7 +17,7 @@ class Race:
     def __init__(self, team1, team2):
         self.team1 = team1
         self.team2 = team2
-        self.score = 0
+        self.scores = [0, 0]
         self.overtakes_team1 = 0
         self.overtakes_team2 = 0
         self.laps = []
@@ -22,7 +29,20 @@ class Race:
     def calculate_score(self):
         for lap in self.laps:
             if lap.speed_team1 > lap.speed_team2:
-                self.score += 1
+                self.scores[0] += 1
             elif lap.speed_team2 > lap.speed_team1:
-                self.score -= 1
+                self.scores[1] += 1
+
+    def print_info(self):
+        print(f"team1 = {self.team1}")
+        print(f"team2 = {self.team2}")
+        print(f"overtakes_team1 = {self.overtakes_team1}")
+        print(f"overtakes_team2 = {self.overtakes_team2}")
+        print(f"scores = {self.scores[0]} : {self.scores[1]}")
+
+        for lap in self.laps:
+            print(f"lap #{lap.number}: ")
+            print(f"speed_team1 = {lap.speed_team1}")
+            print(f"speed_team2 = {lap.speed_team2}")
+            print(f"best_lap = {lap.best_lap}\n")
 
--- main.py
+++ main.py
@@ -3,41 +3,75 @@ from telebot import types
 
 import texts
 from config import TOKEN, CLUBS
+from classes import Race, UserState
 
 bot = telebot.TeleBot(TOKEN)
 
+user_states = {}  # Словарь для хранения состояний пользователей
 
-@bot.message_handler(content_types=['text'])
+
+
 def enter_club(message):
-    bot.send_message(message.from_user.id, texts.enter_club_name)
+    user_id = message.from_user.id
+    user_states[user_id].entering_club = True
+    bot.send_message(user_id, texts.enter_club_name)
 
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)  # создание новых кнопок
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     for club in CLUBS:
         markup.add(types.KeyboardButton(club))
 
-    bot.send_message(message.from_user.id, texts.choose_option, reply_markup=markup)  # ответ бота
-
-    print(f"Выбранный клуб: {message.text}")
+    bot.send_message(user_id, texts.choose_option, reply_markup=markup)
 
 
-@bot.message_handler(content_types=['text'])
+def enter_teams(message, race):
+    user_id = message.from_user.id
+    user_states[user_id].entering_teams = True
+    user_states[user_id].entering_club = False
+
+    bot.send_message(message.from_user.id, texts.enter_race_team1)
+    team1 = message.text
+
+    bot.send_message(message.from_user.id, texts.enter_race_team2)
+    team2 = message.text
+
+    race.team1 = team1
+    race.team2 = team2
+
+
+def enter_overtakes(message, race):
+    user_id = message.from_user.id
+    user_states[user_id].entering_overtakes = True
+    user_states[user_id].entering_teams = False
+
+    bot.send_message(message.from_user.id, texts.enter_race_overtakes1)
+    overtakes_team1 = int(message.text)
+
+    bot.send_message(message.from_user.id, texts.enter_race_overtakes2)
+    overtakes_team2 = int(message.text)
+
+    race.overtakes_team1 = overtakes_team1
+    race.overtakes_team2 = overtakes_team2
+
+
+def enter_laps(message, race):
+    pass
+
+
 def enter_race_data(message):
-    bot.send_message(message.from_user.id, texts.enter_race_data)
+    bot.send_message(message.from_user.id, texts.enter_race)
 
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)  # создание новых кнопок
-    for club in CLUBS:
-        markup.add(types.KeyboardButton(club))
+    race = Race("team1", "team2")
+    enter_teams(message, race)
+    race.print_info()
+    enter_overtakes(message, race)
+    race.print_info()
+    enter_laps(message, race)
 
-    bot.send_message(message.from_user.id, texts.choose_option, reply_markup=markup)  # ответ бота
 
-    print(f"Выбранный клуб: {message.text}")
-
-
-@bot.message_handler(content_types=['text'])
 def add_race_info(message):
     enter_club(message)
 
-    enter_race_data()
+    enter_race_data(message)
 
 
 @bot.message_handler(commands=['start'])
@@ -49,15 +83,31 @@ def start_bot(message):
 
 
 @bot.message_handler(content_types=['text'])
-def get_text_messages(message):
+def handle_messages(message):
+    user_id = message.from_user.id
+
+    print(f"message.text = {message.text}")
+
     if message.text == 'Начать':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)  # создание новых кнопок
+        user_states[user_id] = UserState()
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         btn1 = types.KeyboardButton(texts.add_race_info)
         markup.add(btn1)
-        bot.send_message(message.from_user.id, texts.choose_option, reply_markup=markup)  # ответ бота
+        bot.send_message(user_id, texts.choose_option, reply_markup=markup)
 
     elif message.text == texts.add_race_info:
-        add_race_info()
+        user_states[user_id] = UserState()
+        enter_club(message)
+
+    elif user_id in user_states:
+        state = user_states[user_id]
+
+        if state.entering_club:
+            enter_teams(message, state)
+        elif state.entering_teams:
+            enter_overtakes(message, state)
+        elif state.entering_overtakes:
+            enter_laps(message, state)
 
 
 bot.infinity_polling()  # обязательная для работы бота часть
